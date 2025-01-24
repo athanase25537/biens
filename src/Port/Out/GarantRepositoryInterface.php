840b9cf3b3be5a46b391c2e419bb67e5d63e9847
alfred-uppercase <alfreddjpro@gmail.com>
2025-01-24 11:15:59 +0300

    fix

--- src/Port/Out/GarantRepositoryInterface.php
+++ src/Port/Out/GarantRepositoryInterface.php
@@ -5,7 +5,7 @@ use App\Core\Domain\Entity\Garant;
 
 interface GarantRepositoryInterface
 {
-    public function save(Bail $bail): Garant;
+    public function save(Garant $garant): Garant;
     public function findById(int $id): ?Garant;
     public function findAll(): array;
     public function delete(int $id): bool;

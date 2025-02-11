9cc584ddd074aefd9679c87a24c681ba454a8dd3
Athanase25537 <athanase21.aps2a@gmail.com>
2025-02-11 15:38:07 +0300

    refactor: routes.php

--- src/Route/routes.php
+++ src/Route/routes.php
@@ -12,56 +12,6 @@ use App\Adapter\Api\Rest\SuiviController;
 use App\Adapter\Api\Rest\UserAbonnementController;
 use App\Adapter\Persistence\Stripe\SubscriptionStripe;
 
-$apiKey = $_ENV['STRIPE_SECRET_KEY'];
-$endPointKey = $_ENV['SECRET_KEY'];
-\Stripe\Stripe::setApiKey($apiKey);
-
-$stripeService = new SubscriptionStripe(
-    $apiKey,
-    $endPointKey
-);
-
-try {
-    // 1. Créer le client
-    $customer = $stripeService->createCustomer('client@example.com', 'Jenny Rosen');
-
-    // 2. Définir un moyen de paiement valide
-    $paymentMethods = $stripeService->getStripe()->paymentMethods->create([
-        'type' => 'card',
-        'card' => [
-            'token' => 'tok_visa'
-        ],
-        'billing_details' => ['name' => 'Jenny Rosen'],
-    ]);
-
-    $paymentId = $paymentMethods->id; 
-
-    $paymentMethod = \Stripe\PaymentMethod::retrieve($paymentId);
-    if ($paymentMethod) {
-        $paymentMethod->attach(['customer' => $customer->id]);
-    } else {
-        throw new Exception("Le moyen de paiement est introuvable.");
-    }
-
-    \Stripe\Customer::update($customer->id, [
-        'invoice_settings' => ['default_payment_method' => $paymentId]
-    ]);
-
-    $price = 'price_1QorkEJyueeXhqQIbKDjOlGW';
-    $subscription = $stripeService->createSubscription($customer->id, $price);
-    
-    $created_at = date('Y-m-d H:i:s', $subscription->created);
-    $period_end = date('Y-m-d H:i:s', $subscription->current_period_end);
-    $period_start = date('Y-m-d H:i:s', $subscription->current_period_start);
-    $items = $subscription->items;
-    $plan = $items->data[0]->plan;
-    print($subscription->status);
-    exit();
-
-} catch (\Exception $e) {
-    echo "Erreur : " . $e->getMessage();
-}
-
 // Define routes
 function defineRoutes(
     $router, 

6b8dfc1fd59b37ae34c2a7ac9c72f4298bb7ef36
ANDRIAMASY <andriamasyathanase.nazy@gmail.com>
2025-02-13 10:14:09 +0300

    refactor index.php

--- Public/index.php
+++ Public/index.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once '../vendor/autoload.php';
+require_once dirname(__DIR__) . DIRECTORY_SEPARATOR . 'vendor/autoload.php';
 
 // Adapters
 use App\Adapter\Persistence\MySQLAdapter;

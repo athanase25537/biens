d4dc915d066b41d28422ca321b1a21560c3cd3c0
athanase25537 <andriamasyathanase.nazy@gmail.com>
2025-02-21 14:20:46 +0300

    back to the real database config

--- config/database.php
+++ config/database.php
@@ -1,19 +1,9 @@
 <?php
-// return [
-//     'db_type' => 'mysql', // Peut être 'mysql', 'postgresql', etc.
-//     'host' => '192.168.48.1',
-//     'port'    => '8099',
-//     'dbname' => 'monbail',
-//     'user' => 'monbail',
-//     'password' => 'M7AV9n8lmDijoFMj',
-// ];
-
-
-// for local test
 return [
     'db_type' => 'mysql', // Peut être 'mysql', 'postgresql', etc.
-    'host' => 'db',
-    'dbname' => 'bailonline',
-    'user' => 'root',
-    'password' => '',
+    'host' => '192.168.48.1',
+    'port'    => '8099',
+    'dbname' => 'monbail',
+    'user' => 'monbail',
+    'password' => 'M7AV9n8lmDijoFMj',
 ];

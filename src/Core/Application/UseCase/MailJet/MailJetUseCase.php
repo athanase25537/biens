99d8dbb3da522bdeaad294c1a33201f5e8bdff65
athanase25537 <andriamasyathanase.nazy@gmail.com>
2025-02-21 10:01:05 +0300

    feat: Mailjet and update quittance loyer

---
+++ src/Core/Application/UseCase/MailJet/MailJetUseCase.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Core\Application\UseCase\MailJet;
+
+use App\Port\In\MailJet\MailJetInputPort;
+use \Mailjet\Resources;
+use \Mailjet\Client;
+
+class MailJetUseCase implements MailJetInputPort
+{
+    private Client $mailJet;
+    public function __construct(string $apiKey, string $secretKey)
+    {
+        $this->mailJet = new Client($apiKey, $secretKey, true, ['version' => 'v3.1']);
+    }
+    public function send(array $data): ?array
+    {
+        $body = [
+            'Messages' => [
+                [
+                    'From' => [
+                        'Email' => $data['sender_mail'],
+                        'Name' => $data['sender_name']
+                    ],
+                    'To' => [
+                        [
+                            'Email' => $data['receiver_mail'],
+                            'Name' => $data['receiver_name']
+                        ]
+                    ],
+                    'Subject' => $data['subject'],
+                    'TextPart' => $data['text_part'],
+                    'HTMLPart' => $data['html_part']
+                ]
+            ]
+        ];
+        $response = $this->mailJet->post(Resources::$Email, ['body' => $body]);
+        return $response->success() ? $response->getData() : null;
+    }
+}

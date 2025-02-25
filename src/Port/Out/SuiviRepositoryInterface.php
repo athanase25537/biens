bbf0beab78a421bf35b5419dda6749cbf094e1c9
athanase25537 <andriamasyathanase.nazy@gmail.com>
2025-02-25 08:40:20 +0300

    update: suivi repository interface

--- src/Port/Out/SuiviRepositoryInterface.php
+++ src/Port/Out/SuiviRepositoryInterface.php
@@ -8,6 +8,6 @@ interface SuiviRepositoryInterface
 {
     public function save(Suivi $suivi): Suivi;
     public function update(int $suiviId, array $data): bool;
-    // public function destroy(int $incidentId, int $bienId, int $bailId): bool;
-    // public function getIncident(int $incidentId): ?array;
+    public function destroy(int $suiviId, int $bailId): bool;
+    // public function getSuivi(int $suiviId): ?array;
 }
